a4f2f9f7b93d94aa66be8b3b5c58c2b27dc15d9d
Gavins <wpgavins@iastate.edu>
2017-01-18 10:57:06 -0600

    changed

--- dot.c
+++ dot.c
@@ -1,8 +1,16 @@
 double dot(double a[], double b[], int length) {
 	int i;
-	double sum;   
-	for (i=1; i<= length; i++) { 
+	double sum =0;   
+	for (i=0; i< length; i++) { 
 		sum = sum + a[i]*b[i];
 		}
 	return sum;
 	}
+
+	/*double dot(double a[],double b[],int length){
+		int i;
+		double sum= 0
+		
+		
+		
+	}*/
